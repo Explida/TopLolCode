1ef9d016e80ea4a89e0791d54a9068b702387186
Олексій Комлик < >
2017-04-26 15:57:41 +0300

    Add project files.

---
+++ TopLolCode/UserType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TopLolCode
+{
+    class UserType
+    {
+        public DateTime StartDate;
+        public DateTime EndDate;
+        public DateTime LastAccessDate;
+        public int      DurationTime;
+        public string[] Days = new string[7];
+    }
+}
